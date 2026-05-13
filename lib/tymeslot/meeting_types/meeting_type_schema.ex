5c4363d5ab17f3536194d2bdbe41e7b172989836
Luka Breitig <24662784+wahve3@users.noreply.github.com>
2026-05-13 14:55:23 +0300

    feat(core): embed custom_fields in MeetingTypeSchema
    Changelog: skip

--- lib/tymeslot/meeting_types/meeting_type_schema.ex
+++ lib/tymeslot/meeting_types/meeting_type_schema.ex
@@ -4,6 +4,7 @@ defmodule Tymeslot.MeetingTypes.MeetingTypeSchema do
   """
   use Ecto.Schema
   import Ecto.Changeset
+  alias Tymeslot.CustomFields.FieldDefinition
   alias Tymeslot.Utils.ReminderUtils
   alias Tymeslot.Validation.Constraints
 
@@ -17,6 +18,7 @@ defmodule Tymeslot.MeetingTypes.MeetingTypeSchema do
           allow_video: boolean(),
           sort_order: integer(),
           reminder_config: [map()],
+          custom_fields: [map()],
           user_id: integer() | nil,
           video_integration_id: integer() | nil,
           calendar_integration_id: integer() | nil,
@@ -39,6 +41,8 @@ defmodule Tymeslot.MeetingTypes.MeetingTypeSchema do
     belongs_to(:user, Tymeslot.Auth.UserSchema)
     belongs_to(:video_integration, Tymeslot.Integrations.Video.VideoIntegrationSchema)
     belongs_to(:calendar_integration, Tymeslot.Integrations.Calendar.CalendarIntegrationSchema)
+
+    embeds_many(:custom_fields, FieldDefinition, on_replace: :delete)
 
     timestamps()
   end
@@ -79,6 +83,7 @@ defmodule Tymeslot.MeetingTypes.MeetingTypeSchema do
       :target_calendar_id,
       :reminder_config
     ])
+    |> cast_embed(:custom_fields, with: &FieldDefinition.changeset/2)
     |> validate_required([:name, :duration_minutes, :user_id])
     |> validate_length(:name, Constraints.name_length_opts())
     |> validate_length(:description, max: Constraints.description_max_length())
