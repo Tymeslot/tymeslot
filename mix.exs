90e4973e9638342cb27c0b7f03eb5766b60fd33e
Luka Breitig <24662784+wahve3@users.noreply.github.com>
2026-05-21 13:03:08 +0300

    chore: bump Elixir dependencies
    Security: bandit 1.11.1 (8 CVEs), postgrex 0.22.2 (CVE-2026-32687)
    Updates: ecto_sql 3.14.0, mjml 6.0.0, finch 0.22.0, hammer 7.4.0,
      castore 1.0.19, swoosh 1.25.2, html_sanitize_ex 1.5.1, req 0.5.18,
      floki 0.38.2 and transitive deps (ecto, mint, plug, decimal, etc.)
    
    stripity_stripe update blocked: 3.3.1 requires hackney ~> 4.0,
    incompatible with caldav_client's ~> 1.18 constraint.

--- mix.exs
+++ mix.exs
@@ -116,7 +116,7 @@ defmodule Tymeslot.MixProject do
       {:html_sanitize_ex, "~> 1.4"},
       {:gen_smtp, "~> 1.2"},
       {:castore, "~> 1.0"},
-      {:mjml, "~> 5.2"},
+      {:mjml, "~> 6.0"},
       {:nodejs, "~> 3.0"},
       {:oban, "~> 2.20"},
       {:logger_json, "~> 7.0"},
