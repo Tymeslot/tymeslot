5b437f231f4450d5cc57c9387b2ce81883948c49
Luka Breitig <24662784+wahve3@users.noreply.github.com>
2026-06-24 16:48:38 +0300

    chore: bump version to 1.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tymeslot.MixProject do
   def project do
     [
       app: :tymeslot,
-      version: "1.2.2",
+      version: "1.2.3",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
