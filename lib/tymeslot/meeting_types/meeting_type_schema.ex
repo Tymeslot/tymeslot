a7b23ca87f37c16c8a0353730bfbbf32390e1d13
Luka Breitig <24662784+wahve3@users.noreply.github.com>
2026-08-19 18:05:02 +0300

    feat(core): add an optional slot interval to meeting types
    Changelog: skip
    Signed-off-by: Luka Breitig <24662784+wahve3@users.noreply.github.com>

--- lib/tymeslot/meeting_types/meeting_type_schema.ex
+++ lib/tymeslot/meeting_types/meeting_type_schema.ex
@@ -16,6 +16,7 @@ defmodule Tymeslot.MeetingTypes.MeetingTypeSchema do
           name: String.t() | nil,
           description: String.t() | nil,
           duration_minutes: integer() | nil,
+          slot_interval_minutes: integer() | nil,
           icon: String.t() | nil,
           is_active: boolean(),
           is_private: boolean(),
@@ -45,6 +46,7 @@ defmodule Tymeslot.MeetingTypes.MeetingTypeSchema do
     field(:name, :string)
     field(:description, :string)
     field(:duration_minutes, :integer)
+    field(:slot_interval_minutes, :integer)
     field(:icon, :string)
     field(:is_active, :boolean, default: true)
     field(:is_private, :boolean, default: false)
@@ -125,6 +127,7 @@ defmodule Tymeslot.MeetingTypes.MeetingTypeSchema do
       :name,
       :description,
       :duration_minutes,
+      :slot_interval_minutes,
       :icon,
       :is_active,
       :is_private,
@@ -152,6 +155,7 @@ defmodule Tymeslot.MeetingTypes.MeetingTypeSchema do
     |> validate_length(:name, Constraints.name_length_opts())
     |> validate_length(:description, max: Constraints.description_max_length())
     |> validate_number(:duration_minutes, Constraints.duration_minutes_opts())
+    |> validate_number(:slot_interval_minutes, Constraints.slot_interval_minutes_opts())
     |> validate_number(:sort_order, greater_than_or_equal_to: 0)
     |> validate_booking_limits(:meeting_types)
     |> validate_inclusion(:icon, @valid_icons, message: "must be one of the available icons")
