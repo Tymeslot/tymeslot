40dcef137d2517a73ccfc8e096bfebee37a6f187
Luka Breitig <24662784+wahve3@users.noreply.github.com>
2026-09-17 11:19:52 +0300

    chore: bump version to 1.15.8
    Signed-off-by: Luka Breitig <24662784+wahve3@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tymeslot.MixProject do
   def project do
     [
       app: :tymeslot,
-      version: "1.15.7",
+      version: "1.15.8",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
